7878e02b089fc0b97b403141445065fc7ecd0125
buphmin <buphmin@gmail.com>
2019-05-27 14:42:24 -0600

    moved sort to file, added buildArray

--- bubble-sort-v2.php
+++ bubble-sort-v2.php
@@ -1,45 +1,10 @@
 <?php
-function bubbleSort(array &$a)
-{
-  $len = count($a) - 1;
-  $sorted = false;
-  while (!$sorted) {
-    $sorted = true;
-    for ($i = 0; $i < $len; $i++) {
-      $current = $a[$i];
-      $next = $a[$i + 1];
-      if ( $next < $current ) {
-        $a[$i] = $next;
-        $a[$i + 1] = $current;
-        $sorted = false;
-      }
-    }
-  }
-}
+require_once './bubble-sort-core.php';
 
 $myArray = [];
 $numberPool = 4096;
 
-// add numbers divisible by 2
-for ($x = $numberPool; $x >= 0; $x--) {
-  if ($x % 2 === 0) {
-    $myArray[] = $x;
-  }
-}
-
-// add numbers divisible by 3
-for ($x = $numberPool; $x >= 0; $x--) {
-  if ($x % 3 === 0) {
-    $myArray[] = $x;
-  }
-}
-
-// add numbers divisible by 7
-for ($x = $numberPool; $x >= 0; $x--) {
-  if ($x % 7 === 0) {
-    $myArray[] = $x;
-  }
-}
+$myArray = buildArray($numberPool);
 
 $startTime = hrtime(true);
 bubbleSort($myArray);
